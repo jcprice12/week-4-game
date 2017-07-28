6f2ee5dea823f2f0a948394dd71782048b04adeb
jcprice12 <jcprice12@gmail.com>
2017-07-28 10:14:09 -0400

    bug fixes, refactored code. no more game and game session prototypes. now there is only a game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,24 @@
 /*********************************************************************************
 Character Prototype
 *********************************************************************************/
-function Character(myId, myName, myHealth, myAttackPower, myCounterAttackPower, myImageSource){
+function Character(/*myId,*/ myName, myHealth, myAttackPower, myCounterAttackPower, myImageSource){
 	//public variables
 	this.health = myHealth;
 	this.attackPower = myAttackPower;
 
 	//private variables
-	var charId = myId;
+	//var charId = myId;
 	var name = myName;
 	var counterAttackPower = myCounterAttackPower;
 	var baseAttackPower = myAttackPower;
 	var imageSource = myImageSource;
 
 	//public methods to access private variables
+	/*
 	this.getCharId = function(){
 		return charId;
 	}
+	*/
 
 	this.getName = function(){
 		return name;
@@ -49,106 +51,164 @@ Character.prototype.attack = function(enemy){
 /***********************************************************************************
 GameSession Prototype
 ************************************************************************************/
-function GameSession(playing, hero, enemies){
-	this.playing = true;
-	this.hero = hero;
-	this.enemies = enemies;
-	this.defendingCharacter;
-};
+// function GameSession(playing, hero, enemies){
+// 	this.playing = true;
+// 	this.hero = hero;
+// 	this.enemies = enemies;
+// 	this.defendingCharacter;
+// };
 
-/***********************************************************************************
-Game Prototype
-************************************************************************************/
-function Game(session, characters){
-	this.session = session;
-	this.characters = characters;
+// /***********************************************************************************
+// Game Prototype
+// ************************************************************************************/
+// function Game(session, characters){
+// 	this.session = session;
+// 	this.characters = characters;
 
-};
-Game.prototype.buildCharacterInformationDiv = function(parent,information){
-	var info = $("<div>");
-	info.addClass("infoContainer");
-	info.html(information);
-	parent.append(info);
-}
-Game.prototype.buildCharacterCard = function(parent, character){
-	var card = $("<div>");
-	card.addClass("myCard");
-	card.attr("data-character", character.getCharId());
+// };
+// Game.prototype.buildCharacterInformationDiv = function(parent, information){
+// 	var info = $("<div>");
+// 	info.addClass("infoContainer");
+// 	info.html(information);
+// 	parent.append(info);
+// }
+// Game.prototype.buildCharacterCard = function(parent, id, character){
+// 	var card = $("<div>");
+// 	card.addClass("myCard");
+// 	card.attr("data-character", id);
 
-	var cardWrapper = $("<div>");
-	cardWrapper.addClass("cardWrapper");
+// 	var cardWrapper = $("<div>");
+// 	cardWrapper.addClass("cardWrapper");
 
-	var imageWrapper = $("<div>");
-	imageWrapper.addClass("imageWrapper");
+// 	var imageWrapper = $("<div>");
+// 	imageWrapper.addClass("imageWrapper");
 
-	var image = $("<img>");
-	image.addClass("characterImage");
-	image.attr("src", character.getImageSource());
-	image.attr("alt", character.getName());
-	imageWrapper.append(image);
+// 	var image = $("<img>");
+// 	image.addClass("characterImage");
+// 	image.attr("src", character.getImageSource());
+// 	image.attr("alt", character.getName());
+// 	imageWrapper.append(image);
 
-	var characterInformation = $("<div>");
-	characterInformation.addClass("characterInformation");
-	this.buildCharacterInformationDiv(characterInformation, character.health);
-	this.buildCharacterInformationDiv(characterInformation, character.attackPower);
-	this.buildCharacterInformationDiv(characterInformation, character.getCounterAttackPower());
+// 	var characterInformation = $("<div>");
+// 	characterInformation.addClass("characterInformation");
+// 	this.buildCharacterInformationDiv(characterInformation, character.health);
+// 	this.buildCharacterInformationDiv(characterInformation, character.attackPower);
+// 	this.buildCharacterInformationDiv(characterInformation, character.getCounterAttackPower());
 
-	cardWrapper.append(imageWrapper);
-	cardWrapper.append(characterInformation);
-	card.append(cardWrapper);
-	parent.append(card);
+// 	cardWrapper.append(imageWrapper);
+// 	cardWrapper.append(characterInformation);
+// 	card.append(cardWrapper);
+// 	parent.append(card);
 
-}
-Game.prototype.buildCharacterSelect = function(){
-	var characterSelectContainer = $("#characterSelect");
-	// for(i = 0; i < this.characters.length; i++){
-	// 	this.buildCharacterCard(characterSelectContainer, this.characters[i]);
-	// }
-	for (var id in this.characters) {
-	  if (this.characters.hasOwnProperty(id)) {
-	    this.buildCharacterCard(characterSelectContainer, this.characters[id]);
-	  }
-	}
-}
+// }
+// Game.prototype.buildCharacterSelect = function(){
+// 	var characterSelectContainer = $("#characterSelect");
+// 	// for(i = 0; i < this.characters.length; i++){
+// 	// 	this.buildCharacterCard(characterSelectContainer, this.characters[i]);
+// 	// }
+// 	for (var id in this.characters) {
+// 	  //if (this.characters.hasOwnProperty(id)) {
+// 	    this.buildCharacterCard(characterSelectContainer, id, this.characters[id]);
+// 	  //}
+// 	}
+// }
 
 
 /************************************************************************************
 Executed Code and Event Listeners
 ************************************************************************************/
 //must decide if it's worth it to have both a game and a game session object
-var myCharacters = [
-	new Character("luke-skywalker", "Luke Skywalker", 100, 10, 5, "assets/images/lukeSkywalker.jpg"),
-	new Character("obi-wan-kenobi", "Obi-Wan Kenobi", 50, 20, 20, "assets/images/obiWanKenobi.jpg"),
-	new Character("yoda", "Yoda", 50, 30, 5, "assets/images/yoda.jpg"),
-];
+// var myCharacters = [
+// 	new Character("luke-skywalker", "Luke Skywalker", 100, 10, 5, "assets/images/lukeSkywalker.jpg"),
+// 	new Character("obi-wan-kenobi", "Obi-Wan Kenobi", 50, 20, 20, "assets/images/obiWanKenobi.jpg"),
+// 	new Character("yoda", "Yoda", 50, 30, 5, "assets/images/yoda.jpg"),
+// ];
 
 //this seems dumb (could possible identify characters as indices in an array, avoids this for-loop)
-var myCharactersObj = {};
-for(i = 0; i < myCharacters.length; i++){
-	myCharactersObj[myCharacters[i].getCharId()] = myCharacters[i];
+// var myCharactersObj = {};
+// for(i = 0; i < myCharacters.length; i++){
+// 	myCharactersObj[myCharacters[i].getCharId()] = myCharacters[i];
+// }
+
+var myCharactersObj = {
+	"luke-skywalker" : new Character("Luke Skywalker", 100, 10, 5, "assets/images/lukeSkywalker.jpg"),
+	"obi-wan-kenobi" : new Character("Obi-Wan Kenobi", 50, 20, 20, "assets/images/obiWanKenobi.jpg"),
+	"yoda" : new Character("Yoda", 50, 30, 5, "assets/images/yoda.jpg"),
 }
 
-var myGameSession = new GameSession(false,undefined,undefined);
-var myGame = new Game(myGameSession,myCharactersObj);
-myGame.buildCharacterSelect();
+var game = {
+	characters : myCharactersObj,
+	playing : false,
+	hero : undefined,
+	enemies : undefined,
+	defendingCharacter : undefined,
+
+	buildCharacterInformationDiv: function(parent, information){
+		var info = $("<div>");
+		info.addClass("infoContainer");
+		info.html(information);
+		parent.append(info);
+	},
+
+	buildCharacterCard: function(parent, id, character){
+		var card = $("<div>");
+		card.addClass("myCard");
+		card.attr("data-character", id);
+
+		var cardWrapper = $("<div>");
+		cardWrapper.addClass("cardWrapper");
+
+		var imageWrapper = $("<div>");
+		imageWrapper.addClass("imageWrapper");
+
+		var image = $("<img>");
+		image.addClass("characterImage");
+		image.attr("src", character.getImageSource());
+		image.attr("alt", character.getName());
+		imageWrapper.append(image);
+
+		var characterInformation = $("<div>");
+		characterInformation.addClass("characterInformation");
+		this.buildCharacterInformationDiv(characterInformation, character.health);
+		this.buildCharacterInformationDiv(characterInformation, character.attackPower);
+		this.buildCharacterInformationDiv(characterInformation, character.getCounterAttackPower());
+
+		cardWrapper.append(imageWrapper);
+		cardWrapper.append(characterInformation);
+		card.append(cardWrapper);
+		parent.append(card);
+
+	},
+
+	buildCharacterSelect: function(){
+		var characterSelectContainer = $("#characterSelect");
+		for (var id in this.characters) {
+		    this.buildCharacterCard(characterSelectContainer, id, this.characters[id]);
+		}
+	},
+};
+
+//var myGameSession = new GameSession(false,undefined,undefined);
+//var myGame = new Game(myGameSession,myCharactersObj);
+game.buildCharacterSelect();
 
 //will there be issues with DOM loading like this?
 $( document ).ready(function() {
     $(".myCard").on("click", function(){
     	var myEnemies = [];
     	var myId = $(this).attr("data-character");
-    	myGame.session.playing = true;
-    	for (var id in myGame.characters) {
-			if (myGame.characters.hasOwnProperty(id)) {
+    	game.playing = true;
+    	for (var id in game.characters) {
+			if (game.characters.hasOwnProperty(id)) {
 				if(myId === id) {
-			    	myGame.session.hero = myGame.characters[id];
+			    	game.hero = game.characters[id];
 			  	} else {
-			  		myEnemies.push(myGame.characters[id]);
+			  		myEnemies.push(game.characters[id]);
 			  	}
 			}
 		}
-    	myGame.session.enemies = myEnemies;
-    	console.log(myGame.session);
+    	game.enemies = myEnemies;
+    	console.log(game);
     	//start playing with gameSession
     });
 });
